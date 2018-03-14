e6158734e53d641d1828f6690f7a8ac51b012db9
Tiago Do Couto <cs.couto@outlook.com>
2018-03-14 00:18:59 -0500

    [project] - initial

---
+++ app/src/main/java/com/coutocode/allnet_xchat/ContactListActivity.kt
@@ -0,0 +1,12 @@
+package com.coutocode.allnet_xchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_list)
+    }
+}
