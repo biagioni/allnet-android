67580404207ab800e11b3b53d09c04e7815ca185
Tiago Do Couto <cs.couto@outlook.com>
2018-03-22 23:46:42 -0500

    [contactnewfragment.kt] - implemented binding with layout

--- app/src/main/java/org/alnet/allnet_android/fragments/ContactNewFragment.kt
+++ app/src/main/java/org/alnet/allnet_android/fragments/ContactNewFragment.kt
@@ -3,22 +3,49 @@ package org.alnet.allnet_android.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_contact_new.*
 import org.alnet.allnet_android.R
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.fragment_contact_new.view.*
 
 
-/**
- * A simple [Fragment] subclass.
- */
 class ContactNewFragment : Fragment() {
 
+    var spinner: Spinner? = null
+    var editTextName: EditText? = null
+    var editTextSecret: EditText? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_contact_new, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_contact_new, container, false)
+
+        val adapter = ArrayAdapter.createFromResource(activity,
+                R.array.contact_type, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner = view.spinnerType
+        spinner!!.adapter = adapter
+        editTextName = view.etName
+        editTextSecret = view.etSecret
+
+        view.buttonRequest.setOnClickListener {
+            val name = editTextName!!.text
+            val secret = editTextSecret!!.text
+            val index = spinner!!.selectedItemPosition
+        }
+
+        return view
     }
 
-}// Required empty public constructor
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
