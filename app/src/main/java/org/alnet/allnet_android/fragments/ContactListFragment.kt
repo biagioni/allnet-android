a9fb9347c0ae500746fb7a220cafcab91345b8f2
Tiago Do Couto <cs.couto@outlook.com>
2018-03-26 19:18:16 -0500

    [contactlistfragent.kt] - implemented layout controller

--- app/src/main/java/org/alnet/allnet_android/fragments/ContactListFragment.kt
+++ app/src/main/java/org/alnet/allnet_android/fragments/ContactListFragment.kt
@@ -3,22 +3,45 @@ package org.alnet.allnet_android.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.alnet.allnet_android.R
+import kotlinx.android.synthetic.main.fragment_contact_list.view.*
+import org.alnet.allnet_android.*
 
 
 /**
  * A simple [Fragment] subclass.
  */
-class ContactListFragment : Fragment() {
+class ContactListFragment : Fragment(), INetwork {
+    override fun listContacts(contact: String) {
+        contacts.add(contact)
+    }
 
+    var mRecyclerView: RecyclerView? = null
+    lateinit var networkAPI: NetworkAPI
+    var contacts = ArrayList<String>()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_contact_list, container, false)
+        val view  = inflater!!.inflate(R.layout.fragment_contact_list, container, false)
+        mRecyclerView = view.recyclerView
+        networkAPI = (activity as TabBarActivity).networkAPI!!
+        contacts = (activity as TabBarActivity).contacts
+        networkAPI.listener = this
+
+        updateUI()
+        return view
+    }
+
+
+    fun updateUI(){
+        val layoutManager = LinearLayoutManager(activity)
+        mRecyclerView!!.layoutManager = layoutManager
+        val adapter = ContactAdapter(contacts);
+        mRecyclerView!!.adapter = adapter
     }
 
 }
