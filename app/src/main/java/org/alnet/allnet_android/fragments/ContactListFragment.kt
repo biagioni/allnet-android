139eaf30aecaf2ddb5d47454c1335bad5b66cd4a
Tiago Do Couto <cs.couto@outlook.com>
2018-04-11 20:52:40 -0500

    [contactlistfragment.kt] - implemented function to display settings menu item

--- app/src/main/java/org/alnet/allnet_android/fragments/ContactListFragment.kt
+++ app/src/main/java/org/alnet/allnet_android/fragments/ContactListFragment.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import kotlinx.android.synthetic.main.fragment_contact_list.view.*
 import org.alnet.allnet_android.*
 import org.alnet.allnet_android.activities.MessageActivity
@@ -18,6 +16,8 @@ import org.alnet.allnet_android.model.ContactModel
 
 class ContactListFragment : Fragment(), INetwork, ContactAdapter.ItemClickListener {
 
+    var settings = false
+
     override fun onclick(contact: ContactModel) {
         NetworkAPI.contact = contact.name
         val intent = Intent(activity, MessageActivity::class.java)
@@ -25,15 +25,27 @@ class ContactListFragment : Fragment(), INetwork, ContactAdapter.ItemClickListen
     }
 
     override fun listContactsUpdated() {
-        updateUI()
+        updateUI(NetworkAPI.contacts)
     }
 
-    //TODO notifications for messages
-    override fun listMsgUpdated() {
-
+    override fun listHiddenContactsUpdated() {
+        val contacts = ArrayList<ContactModel>()
+        NetworkAPI.contacts.forEach {
+            contacts.add(it)
+        }
+        contacts.add(ContactModel("","",2))
+        NetworkAPI.hiddencontacts.forEach {
+            contacts.add(it)
+        }
+        updateUI(contacts)
     }
 
     var mRecyclerView: RecyclerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true);
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -41,7 +53,7 @@ class ContactListFragment : Fragment(), INetwork, ContactAdapter.ItemClickListen
         mRecyclerView = view.recyclerView
         NetworkAPI.listener = this
 
-        updateUI()
+        updateUI(NetworkAPI.contacts)
         return view
     }
 
@@ -50,11 +62,31 @@ class ContactListFragment : Fragment(), INetwork, ContactAdapter.ItemClickListen
         NetworkAPI.listener = null
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.menu_contacts, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menuEdit -> {
+                if (!settings){
+                    NetworkAPI.hiddencontacts.clear()
+                    NetworkAPI.getHiddenContacts()
+                    settings = true
+                }else {
+                    settings = false
+                    updateUI(NetworkAPI.contacts)
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
-    fun updateUI(){
+
+    fun updateUI(contacts: ArrayList<ContactModel>){
         val layoutManager = LinearLayoutManager(activity)
         mRecyclerView!!.layoutManager = layoutManager
-        val adapter = ContactAdapter(NetworkAPI.contacts, this);
+        val adapter = ContactAdapter(contacts, this);
         mRecyclerView!!.adapter = adapter
     }
 
