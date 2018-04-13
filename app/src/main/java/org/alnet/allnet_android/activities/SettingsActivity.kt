08b860fb49cfd0eb49bba8474a6e159bf77be30a
Tiago Do Couto <cs.couto@outlook.com>
2018-04-13 14:49:41 -0500

    [activity_settings.xml] - deseigned layout

---
+++ app/src/main/java/org/alnet/allnet_android/activities/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package org.alnet.allnet_android.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.alnet.allnet_android.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
