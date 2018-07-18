989c3777af2a9b10efd0e33b607517df6f4dddde
edo <esb@hawaii.edu>
2018-07-17 17:47:53 -1000

    removed traced.c and the files in abc

--- app/src/main/cfiles/abc/abc-iw.h
+++
@@ -1,9 +0,0 @@
-#ifndef ABC_IW_H
-#define ABC_IW_H
-
-#include "abc-wifi.h" /* abc_wifi_config_iface */
-
-/** public iw-based wifi config interface, ready to use */
-extern abc_wifi_config_iface abc_wifi_config_iw;
-
-#endif /* ABC_IW_H */
